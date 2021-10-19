23293079a61cb6172106f8b20e04d7b0b8fe008f
lukatchubabria <lukatchubabria@gmail.com>
2021-10-19 19:06:12 +0400

    add empty cart string

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -5,12 +5,15 @@ const Cart = (props) => {
 
     return (
         <div style={props.openCart ? {display: "block"} : {display: "none"}} className="cart">
-            {props.cartList.map((list, id)=> (
-                <div key={id} className="cart__list">
-                    <p>დომენი: {list.domainName}{list.domainExtension}</p>
-                    <p>ფასი: {list.price}</p>
-                </div>
-            ))}
+            {props.cartList.length === 0
+                ? <p>კალათი ცარიელია</p> 
+                : props.cartList.map((list, id)=> (
+                    <div key={id} className="cart__list">
+                        <p>დომენი: {list.domainName}{list.domainExtension}</p>
+                        <p>ფასი: {list.price}</p>
+                    </div>
+                ))
+            }
         </div>
     )
 }
